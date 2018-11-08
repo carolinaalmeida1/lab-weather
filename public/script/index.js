b579e0882889e148c743c0be18ac1f714ae5eb63
Letícia Faria <leticianfaria@hotmail.com>
2018-11-08 19:24:53 -0200

    Google maps API added

--- public/script/index.js
+++ public/script/index.js
@@ -1,8 +1,41 @@
 const MONTHS = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
 
-$(document).ready( () => {
+$(document).ready(() => {
   $("#current-weather").on("click", () => currentWeatherInfo());
   $("#five-days").on("click", () => weatherInfo5Days());
+
+  function initMap(lat, lng) {
+    var currentLocation = { lat: lat, lng: lng };
+    var map = new google.maps.Map(
+      document.getElementById('map'), { zoom: 16, center: currentLocation });
+    var marker = new google.maps.Marker({ position: currentLocation, map: map });
+  }
+
+  window.navigator.geolocation.getCurrentPosition(function (position) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&APPID=e3747c4f5aa0b546e718e6ca89ace477`)
+      .then(response => response.json())
+      .then(data => {
+        const date = new Date();
+        const hourDiaAtual = date.getHours();
+        const dayDiaAtual = date.getDate();
+        const month = MONTHS[date.getMonth()];
+
+        $(".info").append(`
+        <div>
+          <h1>${dayDiaAtual} de ${month}</h1>
+          <i>${data.name} - ${data.sys.country}</i>
+          <h2>${hourDiaAtual}h</h2>
+          <h3>${Math.round(data.main.temp)}°C Atual</h3>
+          <h3>${Math.round(data.main.temp_min)}°C Mínima</h3>
+          <h3>${Math.round(data.main.temp_max)}°C Máxima</h3>
+
+          <button id="more-details">VER MAIS</button>
+        </div>
+        `);
+        $("#more-details").one("click", () => weatherDetails(data));
+        initMap(position.coords.latitude, position.coords.longitude);
+      })
+  });
 });
 
 function currentWeatherInfo() {
@@ -13,12 +46,12 @@ function currentWeatherInfo() {
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=e3747c4f5aa0b546e718e6ca89ace477`)
       .then(response => response.json())
       .then(data => {
-          const date = new Date();
-          const hourDiaAtual = date.getHours();
-          const dayDiaAtual = date.getDate();
-          const month = MONTHS[date.getMonth()];
+        const date = new Date();
+        const hourDiaAtual = date.getHours();
+        const dayDiaAtual = date.getDate();
+        const month = MONTHS[date.getMonth()];
 
-          $(".info").append(`
+        $(".info").append(`
           <div>
             <h1>${dayDiaAtual} de ${month}</h1>
             <i>${data.name} - ${data.sys.country}</i>
@@ -30,13 +63,17 @@ function currentWeatherInfo() {
             <button id="more-details">VER MAIS</button>
           </div>
           `);
-
-          $("#more-details").on("click", () => weatherDetails(data));
+        function currentMap(lat, lng) {
+          var currentLocation = { lat: lat, lng: lng };
+          var map = new google.maps.Map(
+            document.getElementById('map'), { zoom: 16, center: currentLocation });
+          var marker = new google.maps.Marker({ position: currentLocation, map: map });
+        }
+        currentMap(data.coord.lat, data.coord.lon);
+        $("#more-details").one("click", () => weatherDetails(data));
       })
   }
 }
-
-
 
 function weatherInfo5Days() {
   $(".info").html("");
@@ -46,12 +83,12 @@ function weatherInfo5Days() {
     fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&APPID=e3747c4f5aa0b546e718e6ca89ace477`)
       .then(response => response.json())
       .then(data => { createAll(data.list) }
-    )
+      )
   }
 }
 
 function createAll(weatherListData) {
-  weatherListData.reduce(function(diaAnterior, diaAtual, index) {
+  weatherListData.reduce(function (diaAnterior, diaAtual, index) {
 
     const dateDiaAtual = new Date(diaAtual.dt_txt);
     const hourDiaAtual = dateDiaAtual.getHours();
@@ -108,3 +145,4 @@ function weatherDetails(data) {
         <h2>Vento: ${Math.round(data.wind.speed * 3.66)} km/h</h2>       
       `);
 }
+
