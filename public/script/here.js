ccbab916d7687fb176150b4349ffc3207e9df78d
carolinaalmeida1 <carolina.almeida1@outlook.com>
2018-11-08 16:05:31 -0200

    api here com busca por texto

--- public/script/here.js
+++ public/script/here.js
@@ -1,51 +1,65 @@
-// Instantiate a map and platform object:
-var platform = new H.service.Platform({
-  'app_id': 'SdALrBLw0Rp5lUtqlqWn',
-  'app_code': '4fG6dYXr9-WVrTq85Ebi3Q'
-});
-// Retrieve the target element for the map:
-var targetElement = document.getElementById('mapContainer');
-
-// Get default map types from the platform object:
-var defaultLayers = platform.createDefaultLayers();
-
-// Instantiate the map:
-var map = new H.Map(
-  document.getElementById('mapContainer'),
-  defaultLayers.normal.map,
-  {
-  zoom: 10,
-  center: { lat: -23.5505, lng: -46.6333 }
+$(document).ready(function () {
+  var platform = new H.service.Platform({
+    "app_id": "SdALrBLw0Rp5lUtqlqWn",
+    "app_code": "4fG6dYXr9-WVrTq85Ebi3Q"
   });
 
-// Create the parameters for the geocoding request:
-var geocodingParams = {
-    searchText: 'Avenida Paulista, 500, São Paulo'
-  };
+  var targetElement = document.getElementById("mapContainer");
 
-// Define a callback function to process the geocoding response:
-var onResult = function(result) {
-  var locations = result.Response.View[0].Result,
-    position,
-    marker;
-    // console.log(result.Response.View[0].Result)
-  // Add a marker for each location found
-  for (i = 0;  i < locations.length; i++) {
-  position = {
-    lat: locations[i].Location.DisplayPosition.Latitude,
-    lng: locations[i].Location.DisplayPosition.Longitude
-  };
-  marker = new H.map.Marker(position);
-  map.addObject(marker);
+  var defaultLayers = platform.createDefaultLayers();
+
+  $(".search-btn").click(function () {
+    getLocation();
+  });
+
+  const geocoder = platform.getGeocodingService();
+
+  function getLocation() {
+    const startVal = $(".search").val();
+    const geocodingParams = {
+      searchText: startVal
+    };
+
+    return geocoder.geocode(geocodingParams, getStartGeoLocation, function (e) {
+      alert(e);
+    });
   }
-};
 
-// Get an instance of the geocoding service:
-var geocoder = platform.getGeocodingService();
+  function getStartGeoLocation(result) {
+    const locations = result.Response.View[0].Result;
 
-// Call the geocode method with the geocoding parameters,
-// the callback and an error callback function (called if a
-// communication error occurs):
-geocoder.geocode(geocodingParams, onResult, function(e) {
-  alert(e);
+    const localStartLat = locations[0].Location.DisplayPosition.Latitude;
+    const localStartLng = locations[0].Location.DisplayPosition.Longitude;
+
+    return drawMap(localStartLat, localStartLng);
+  };
+
+  function drawMap(s, e) {
+    $("#mapContainer").html("");
+
+    const map = new H.Map(
+      targetElement,
+      defaultLayers.normal.map,
+      {
+        zoom: 14,
+        center: { lat: s, lng: e }
+      }
+    );
+
+    const onResult = function (result) {
+      let route,
+        startPoint;
+
+      route = result.response
+
+      startPoint = route.waypoint[0].mappedPosition;
+
+      const startMarker = new H.map.Marker({
+        lat: startPoint.latitude,
+        lng: startPoint.longitude
+      });
+
+      map.addObjects(startMarker);
+    };
+  };
 });
